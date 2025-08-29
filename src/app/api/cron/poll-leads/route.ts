4ce58871fd242c8e4ae91e9876aaae17a07f6b01
Arda Basoglu <ardabasoglu@gmail.com>
2025-08-29 13:52:09 +0300

    refactor(notifications): enhance email content for merchant funding applications by adding specific instructions and contact details for improved user guidance

--- src/app/api/cron/poll-leads/route.ts
+++ src/app/api/cron/poll-leads/route.ts
@@ -79,15 +79,18 @@ export async function POST(request: NextRequest) {
             const emailResult = await notificationService.sendEmail({
               to: lead.email,
               subject: "Complete Your Merchant Funding Application",
-              text: `Hi ${leadName},\n\nThank you for requesting information on merchant funding for your business. My name is Ryan and I’ll be assisting you through the process. Please click on the link below to complete your application:\n\n${intakeUrl}\n\nThis secure link will allow you to provide the required information and upload necessary documents.\n\nIf you have any questions, please don't hesitate to contact us.\n\nBest regards,\nMerchant Funding Team`,
+              text: `Hi ${leadName},\n\nThank you for requesting information on merchant funding for your business. My name is Ryan and I'll be assisting you through the process. Please click on the link below to complete your application:\n\n${intakeUrl}\n\nThis secure link will allow you to provide the required information and upload necessary documents. Please note that you will be prompted to submit the last 3 months' business bank statements.\n\nPlease call or email if you have any questions, otherwise we will be in touch shortly.\n\nBest Regards,\n\nRyan\nFunding Specialist\n\nsupport@merchantfunding.com\n\n+1 888-867-3087`,
               html: `
                 <h2>Complete Your Merchant Funding Application</h2>
                 <p>Hi ${leadName},</p>
-                <p>Thank you for requesting information on merchant funding for your business. My name is Ryan and I’ll be assisting you through the process. Please click on the link below to complete your application:</p>
+                <p>Thank you for requesting information on merchant funding for your business. My name is Ryan and I'll be assisting you through the process. Please click on the link below to complete your application:</p>
                 <p><a href="${intakeUrl}" style="background-color: #28a745; color: white; padding: 10px 20px; text-decoration: none; border-radius: 5px;">Click here to complete your application</a></p>
-                <p>This secure link will allow you to provide the required information and upload necessary documents.</p>
-                <p>If you have any questions, please don't hesitate to contact us.</p>
-                <p>Best regards,<br>Merchant Funding Team</p>
+                <p>This secure link will allow you to provide the required information and upload necessary documents. Please note that you will be prompted to submit the last 3 months' business bank statements.</p>
+                <p>Please call or email if you have any questions, otherwise we will be in touch shortly.</p>
+                <p>Best Regards,</p>
+                <p>Ryan<br>Funding Specialist</p>
+                <p><a href="mailto:support@merchantfunding.com">support@merchantfunding.com</a></p>
+                <p><a href="tel:+18888673087">+1 888-867-3087</a></p>
               `,
               leadId: lead.id,
             });
